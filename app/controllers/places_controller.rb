977ea81805a4d9188008bc119fa0cfffd4632856
Michael Critelli <mcritelli25@aol.com>
2015-06-15 23:57:56 +0000

    Activate the links in the top navigation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,6 @@
 class PlacesController < ApplicationController
 	def index
-		@places = Place.all
-		@places = Place.paginate(:page => params[:page])
+		@places = Place.order(:name).page(params[:page]).per(9)
 	end
 
 	def new
