5a95a81c3496efca8d8cb3ed6fcaf4ae49b52027
Michael Critelli <mcritelli25@aol.com>
2015-06-16 00:05:23 +0000

    adding user authentication to the application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Nomster1::Application.configure do
+    config.action_mailer.default_url_options = { :host => 'nomster1-michael-critelli.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
